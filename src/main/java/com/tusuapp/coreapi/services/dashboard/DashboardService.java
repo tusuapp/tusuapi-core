32051b65265b30261f249d7cd9b8f98e4f713208
coderithik@gmail.com <coderithik@gmail.com>
2025-07-01 23:34:01 +0530

    Credit point return with tutor dashboard

--- src/main/java/com/tusuapp/coreapi/services/dashboard/DashboardService.java
+++ src/main/java/com/tusuapp/coreapi/services/dashboard/DashboardService.java
@@ -2,14 +2,18 @@ package com.tusuapp.coreapi.services.dashboard;
 
 import com.tusuapp.coreapi.constants.BookingConstants;
 import com.tusuapp.coreapi.models.BookingRequest;
+import com.tusuapp.coreapi.models.CredPointMaster;
 import com.tusuapp.coreapi.models.dtos.bookings.BookingRequestDto;
 import com.tusuapp.coreapi.repositories.BookingRequestRepo;
+import com.tusuapp.coreapi.repositories.CreditPointRepo;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 import static com.tusuapp.coreapi.utils.SessionUtil.getCurrentUserId;
 import static com.tusuapp.coreapi.utils.converters.TimeZoneConverter.getCurrentUTCTime;
@@ -22,6 +26,8 @@ public class DashboardService {
     @Autowired
     private BookingRequestRepo bookingsRepo;
 
+    @Autowired
+    private CreditPointRepo creditPointRepo;
 
     public ResponseEntity<?> getTutorDashboard() {
         Long tutorId = getCurrentUserId();
@@ -38,7 +44,9 @@ public class DashboardService {
                 );
         List<BookingRequestDto> upcomingDto = upcomingAccepted.stream().map(BookingRequestDto::fromBookingRequest).toList();
         response.put("upcomingClasses", upcomingDto);
+        Optional<CredPointMaster> credits = creditPointRepo.findByUserId(getCurrentUserId());
         response.put("totalEarning",40);
+        credits.ifPresent(credPointMaster -> response.put("totalEarning", credPointMaster.getBalance()));
         return ResponseEntity.ok(response.toMap());
 
     }
